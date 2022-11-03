9007527c2ac20e0889df33991b9c067904884de5
Hackashi <nexagray@gmail.com>
2022-11-03 15:17:09 +0100

    initial commit

---
+++ resources/views/partials/home/cote.blade.php
@@ -0,0 +1,10 @@
+<section class="site-section section-quote text-center">
+
+    <div class="container">
+
+        <p class="quote">I owe my success to having listened respectfully to the very best advice, and then going away and doing the exact opposite</p>
+        <p class="quote-owner yellow-text">G. K. Chesterton</p>
+        
+    </div>
+    
+</section><!-- /.section-quote -->
